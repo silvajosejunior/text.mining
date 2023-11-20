d2f8d8875f148f7e650f8a042915e77b6481d76f
jose junior <josejunioroliveirasilva@gmail.com>
2023-11-20 10:49:32 -0400

    Add files via upload

---
+++ mineracao.py
@@ -0,0 +1,118 @@
+
+
+
+import nltk
+from dados import base,baseteste,basetreinamento
+#nltk.download()
+
+stopwords = ['a', 'agora', 'algum', 'alguma', 'aquele', 'aqueles', 'de', 'deu', 'do', 'e', 'estou', 'esta', 'esta',
+             'ir', 'meu', 'muito', 'mesmo', 'no', 'nossa', 'o', 'outro', 'para', 'que', 'sem', 'talvez', 'tem', 'tendo',
+             'tenha', 'teve', 'tive', 'todo', 'um', 'uma', 'umas', 'uns', 'vou']
+
+stopwordsnltk = nltk.corpus.stopwords.words('portuguese')
+stopwordsnltk.append('vou')
+stopwordsnltk.append('tão')
+
+def removestopwords(texto):
+    frases = []
+    for (palavras, emocao) in texto:
+        semstop = [p for p in palavras.split() if p not in stopwordsnltk]
+        frases.append((semstop, emocao))
+    return frases
+
+def aplicastemmer(texto):
+    stemmer = nltk.stem.RSLPStemmer()
+    frasessstemming = []
+    for (palavras, emocao) in texto:
+        comstemming = [str(stemmer.stem(p)) for p in palavras.split() if p not in stopwordsnltk]
+        frasessstemming.append((comstemming, emocao))
+    return frasessstemming
+
+frasescomstemmingtreinamento = aplicastemmer(basetreinamento)
+frasescomstemmingteste = aplicastemmer(baseteste)
+
+def buscapalavras(frases):
+    todaspalavras = []
+    for (palavras, emocao) in frases:
+        todaspalavras.extend(palavras)
+    return todaspalavras
+
+palavrastreinamento = buscapalavras(frasescomstemmingtreinamento)
+palavrasteste = buscapalavras(frasescomstemmingteste)
+
+def buscafrequencia(palavras):
+    palavras = nltk.FreqDist(palavras)
+    return palavras
+
+frequenciatreinamento = buscafrequencia(palavrastreinamento)
+frequenciateste = buscafrequencia(palavrasteste)
+
+def buscapalavrasunicas(frequencia):
+    freq = frequencia.keys()
+    return freq
+
+palavrasunicastreinamento = buscapalavrasunicas(frequenciatreinamento)
+palavrasunicasteste = buscapalavrasunicas(frequenciateste)
+
+def extratorpalavras(documento):
+    doc = set(documento)
+    caracteristicas = {}
+    for palavras in palavrasunicastreinamento:
+        caracteristicas['%s' % palavras] = (palavras in doc)
+    for palavras in palavrasunicasteste:
+        caracteristicas['%s' % palavras] = (palavras in doc)
+    return caracteristicas
+
+def classificar_texto(texto, classificador):
+    stemmer = nltk.stem.RSLPStemmer()
+    palavras = [str(stemmer.stem(p)) for p in texto.split()]
+    caracteristicas = extratorpalavras(palavras)
+
+    resultado = classificador.classify(caracteristicas)
+    return resultado
+
+caracteristicasfrase = extratorpalavras(['am', 'nov', 'dia'])
+
+basecompletatreinamento = nltk.classify.apply_features(extratorpalavras, frasescomstemmingtreinamento)
+basecompletateste = nltk.classify.apply_features(extratorpalavras, frasescomstemmingteste)
+
+# constroi a tabela de probabilidade
+classificador = nltk.NaiveBayesClassifier.train(basecompletatreinamento)
+#print(classificador.labels())                               #!
+#print(nltk.classify.accuracy(classificador, basecompletateste)) #!
+
+erros = []
+for (frase, classe) in basecompletateste:
+    resultado = classificador.classify(frase)
+    if resultado != classe:
+        erros.append((classe, resultado, frase))
+
+from nltk.metrics import ConfusionMatrix
+esperado = []
+previsto = []
+for (frase, classe) in basecompletateste:
+    resultado = classificador.classify(frase)
+    previsto.append(resultado)
+    esperado.append(classe)
+
+matriz = ConfusionMatrix(esperado, previsto)
+##print(matriz)
+
+teste = input()
+
+testestemming = []
+stemmer = nltk.stem.RSLPStemmer()
+for (palavrastreinamento) in teste.split():
+    comstem = [p for p in palavrastreinamento.split()]
+    testestemming.append(str(stemmer.stem(comstem[0])))
+
+novo = extratorpalavras(testestemming)
+
+distribuicao = classificador.prob_classify(novo)
+for classe in distribuicao.samples():
+
+#print( "%s:%.2f%%" % (classe,distribuicao.prob(classe)))
+        print(f"{classe}:{distribuicao.prob(classe) * 100:.2f}%")
+
+
+
